e09ab6b15bf9b847116f25143b3d5cc720fd2d0d
Harsh Shah <harshdev624@gmail.com>
2023-10-08 20:54:54 +0530

    Changes in About

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,21 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+      A passionate Frontend developer from India studying Computer Engineering at PDEU.{" "}
+        I am a driven and motivated developer with a passion for solving complex problems & creating elegant and efficient solutions.{" "}
         <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        problem-solving aspect.
+        <br/>
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        chess, reading, and watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span> and dedicated to stay up-to-date with the latest industry trends and always seek new challenges to expand my skillset.
+        <br />
+        I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
       </p>
     </motion.section>
   );
