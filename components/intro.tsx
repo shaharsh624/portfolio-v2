0f04a3e6a04dcd07fc8aa55ba3ce0374a1b3bc32
Harsh Shah <harshdev624@gmail.com>
2023-10-21 10:20:33 +0530

    Changes in Intro Header

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,9 +89,14 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <a
+        {/* <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/Harsh Shah Resume.pdf"
+          download
+        > */}
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/"
           download
         >
           Download CV{" "}
