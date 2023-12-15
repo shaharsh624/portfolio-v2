df3046ea28706b8a48d7333a0b496aa139e53274
Harsh Shah <harshdev624@gmail.com>
2023-12-15 17:55:03 +0530

    Update resume link in Intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -96,7 +96,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://drive.google.com/file/d/1QvZ-27TeYHU-kI3YSzfQ2f-N8gqfdbsv/view?usp=sharing"
+          href="https://drive.google.com/file/d/1HbjmZ7IPBs3G88PhYMoJp3b030yLaLxK/view?usp=sharing"
           target="_blank"
         >
           View Resume{" "}
